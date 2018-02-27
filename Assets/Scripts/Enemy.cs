5a02e0eb59fa29d30dd0e1b80be59434b7821099
Kerry Cheng <kerryc635@gmail.com>
2018-02-26 19:10:41 -0800

    enemy script now modified for two players. Will follow closest player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Enemy : MonoBehaviour {
 
@@ -10,41 +11,52 @@ public class Enemy : MonoBehaviour {
 
     private float damageCoolDown = 1f;
     private bool doKnockback = false;
+
+    private int numPlayers = 2;
+    private GameObject[] allPlayers;
     private GameObject player;
     private Rigidbody2D rb2d;
 
     void Awake() {
+        allPlayers = GameObject.FindGameObjectsWithTag("Player");
         player = GameObject.FindGameObjectWithTag("Player");
         rb2d = GetComponent<Rigidbody2D>();
     }
 
     void Update() {
-        if(!doKnockback)
+        //if not being knocked back, erase all forces
+        if (!doKnockback)
             rb2d.velocity = new Vector2(0, 0);
 
+        //when health is 0
         if (health <= 0)
             Destroy(this.gameObject);
 
-        Vector3 detectDistance = transform.position - player.transform.position;
-        //Debug.Log(detectDistance);
+        //checks for player that is closest to enemy (might need to be optimized)
+        float[] playerDistance = new float[numPlayers];
+        for (int i = 0; i < numPlayers; ++i) {
+            playerDistance[i] = Vector2.Distance(allPlayers[i].transform.position, transform.position);
+        }
+        player = allPlayers[playerDistance.ToList().IndexOf(playerDistance.Min())];
+
         //moves toward player until it reaches certain distance
         transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
     }
 
     void OnCollisionEnter2D(Collision2D other) {
         //only take damage if player is attacking, otherwise player is damaged
-        if (other.gameObject.tag == "Player" && player.GetComponent<PlayerController>().attack) {
-            --health;
-            doKnockback = true;
-            //allow for enemies to get knocked back upon getting hit
-            var force = transform.position - other.transform.position;
-            force.Normalize();
-            rb2d.AddForce(force * knockback);
-            Invoke("StopForce", 0.2f);
-        }
-        else if (other.gameObject.tag == "Player") {
-            //stop moving if collided with idle player (currently not working)
-            rb2d.velocity = new Vector2(0, 0);
+        if (other.gameObject.tag == "Player") {
+            //TODO: optimize script to allow for ease of changes if adding more players
+            if ((other.gameObject.GetComponent<PlayerController>() != null && other.gameObject.GetComponent<PlayerController>().attack) ||
+                (other.gameObject.GetComponent<Player2Controller>() != null && other.gameObject.GetComponent<Player2Controller>().attack)) {
+                --health;
+                doKnockback = true;
+                //allow for enemies to get knocked back upon getting hit
+                var force = transform.position - other.transform.position;
+                force.Normalize();
+                rb2d.AddForce(force * knockback);
+                Invoke("StopForce", 0.2f);
+            }
         }
     }
 
